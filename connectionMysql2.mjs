f606761fed3155b626ef0fd445034c0a862b4ea4
pepepinguino69 <105870106+pepepinguino69@users.noreply.github.com>
2022-11-07 07:39:02 -0300

    Create connectionMysql2.mjs

---
+++ connectionMysql2.mjs
@@ -0,0 +1,23 @@
+
+export const optionsMysql = {
+	client: "mysql2",
+	connection: {
+		host: "sql10.freemysqlhosting.net",
+    port:3306,
+		user: "sql10530656",
+		password: "VVbEIRHtxJ",
+		database: "sql10530656",
+   
+	},
+};
+export const optionsMysql = {
+  client: "mysql2",
+  connection: {
+    host: "sql10.freemysqlhosting.net",
+    port:3306,
+    user: "sql10530656",
+    password: "VVbEIRHtxJ",
+    database: "sql10530656",
+   
+  },
+};
